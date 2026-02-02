ecca600ca62e07b4f7a1ff9e199d733a0d574c63
Mayo Layo <l.t05@outlook.com>
2026-02-02 01:01:52 +0100

    implemented is_monotonic test

--- helpers/test_helper_function.py
+++ helpers/test_helper_function.py
@@ -1,5 +1,5 @@
 import unittest
-from helper_functions import _load_new_with_0
+from helper_functions import _load_new_with_0, is_monotonic
 
 class TestHelper(unittest.TestCase):
 
@@ -11,6 +11,15 @@ class TestHelper(unittest.TestCase):
         
         self.assertEqual(test_case_1, [[1, 2, 3, 4],[0, 0, 0 ,0]])
         self.assertEqual(test_case_2, [[1, 2, 3, 4],[1, 2, 3, 4], [0, 0, 0, 0]])
+
+    
+    def test_is_monotonic(self):
+        test_case_1 = [3, 6, 7, 8, 9]
+        test_case_2 = [2, 1, 0]
+        test_case_3 = [1, 2, 2, 3]
+        self.assertTrue(is_monotonic(test_case_1))
+        self.assertFalse(is_monotonic(test_case_2))
+        self.assertFalse(is_monotonic(test_case_3))
 
 
 if __name__ == '__main__':
