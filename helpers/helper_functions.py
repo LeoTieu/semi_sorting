5a4181659b0d823238dc9f45d61ea2cf4ecfbebf
Mayo Layo <l.t05@outlook.com>
2026-02-02 01:01:30 +0100

    Implementation of is_monotonic function

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -9,3 +9,15 @@ def _load_new_with_0(input_list: list, inner_size: int) -> None:
 
     current_size = len(input_list)
     input_list.append([0 for _ in range(inner_size)])
+
+
+def is_monotonic(input_list: list) -> bool:
+    """
+    Returns true if list is monotonic
+    
+    :param input_list: Description
+    :type input_list: list
+    :return: Description
+    :rtype: bool
+    """
+    return all(input_list[index] < input_list[index + 1] for index in range(len(input_list) - 1))
